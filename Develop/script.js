f8495a23fb42cf914c1be33f70f2b4528895728e
spencercreer <spencercreer@gmail.com>
2020-12-19 09:39:22 -0700

    Added responses and password for loop.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+  // Uppercase and lowercase array length 26
+  var UpCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var LowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  // Special character array length 30
+  var SpeChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,6 +18,7 @@ function writePassword() {
 
 function generatePassword(){
   run = confirm("Would you like to create a password? Yes(OK) No(Cancel)");
+  var password = "";
 
   while(run == true){
 
@@ -25,9 +32,36 @@ function generatePassword(){
 
     if(LowC == true || UpC == true || Num == true || SpeC == true){
       for(i = 0; i < length; i++){
-        rand = parseInt(Math.floor(Math.random * 3));
-        console.log(rand);
-        alert("Password generated");
+        rand = Math.ceil(Math.random() * 4);
+        console.log(rand)
+        if(rand === 1){
+          //Uppercase letter
+          var x = Math.floor(Math.random() * UpCase.length);
+
+          password +=  UpCase[x];
+          console.log(password);
+        }
+        else if(rand === 2){
+          //Lowercase letter
+          var x = parseInt(Math.floor(Math.random() * LowCase.length));
+
+          password +=  LowCase[x];
+          console.log(password);
+        }
+        else if(rand === 3){
+          //Lowercase letter
+          var x = Math.floor(Math.random() * 10);
+
+          password += x;
+          console.log(password);
+        }
+        else{
+         //Lowercase letter
+          var x = Math.ceil(Math.random() * SpeChar.length);
+
+          password +=  SpeChar[x];
+          console.log(password);
+        }
       }
     }
     else{
@@ -40,8 +74,8 @@ function generatePassword(){
     run = confirm("Would you like to create a password? Yes(OK) No(Cancel)");
   }
   }
+  return password
 }
-
 
 document.getElementById("generate").addEventListener("click", generatePassword);
 // Add event listener to generate button
