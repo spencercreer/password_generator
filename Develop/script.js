29b8b1a0dfe6f3206c5c61a6ee5ef2f965d6b746
spencercreer <spencercreer@gmail.com>
2020-12-19 14:08:21 -0700

    fixed error that called generatePassword function twice

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-  // Uppercase and lowercase array length 26
+  // Uppercase, lowercase, and special character arrays
   var UpCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var LowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  // Special character array length 30
   var SpeChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 // Write password to the #password input
@@ -16,67 +15,77 @@ function writePassword() {
 
 }
 
+//generatePassword function
 function generatePassword(){
-  run = confirm("Would you like to create a password? Yes(OK) No(Cancel)");
+  var run = true;
   var password = "";
 
-  while(run == true){
+  while(run === true){
 
-  length = prompt("Enter your desired password length. (Enter a number from 8 to 128 characters)");
+    // Prompt user for password length
+    length = prompt("Enter your desired password length. (Enter a number from 8 to 128 characters)");
+  
+    // Password length was correct
+    if(length >= 8 && length <= 128){
 
-  if(length >= 8 && length <= 128){
-    LowC = confirm("Lowercase? Yes(OK) No(Cancel)");
-    UpC = confirm("Uppercase? Yes(OK) No(Cancel)");
-    Num = confirm("Numeric? Yes(OK) No(Cancel)");
-    SpeC = confirm("Special characters? Yes(OK) No(Cancel)");
+      // Confirm user's desired password characters
+      LowC = confirm("Lowercase? Yes(OK) No(Cancel)");
+      UpC = confirm("Uppercase? Yes(OK) No(Cancel)");
+      Num = confirm("Numeric? Yes(OK) No(Cancel)");
+      SpeC = confirm("Special characters? Yes(OK) No(Cancel)");
 
-    if(LowC == true || UpC == true || Num == true || SpeC == true){
-      for(i = 0; i < length; i++){
-        rand = Math.ceil(Math.random() * 4);
-        console.log(rand)
-        if(rand === 1){
-          //Uppercase letter
-          var x = Math.floor(Math.random() * UpCase.length);
+      // User selected at least one character
+      if(LowC == true || UpC == true || Num == true || SpeC == true){
+        for(i = 0; i < length; i++){
+          
+          rand = Math.ceil(Math.random() * 4);
+          x = true;
 
-          password +=  UpCase[x];
-          console.log(password);
+          while(x == true){
+            if(rand === 1 && UpC == true){
+              //Uppercase letter
+              password +=  UpCase[Math.floor(Math.random() * UpCase.length)];
+              x = false;
+            }
+            else if(rand === 2 && LowC == true){
+              //Lowercase letter
+              password +=  LowCase[Math.floor(Math.random() * LowCase.length)];
+              x = false;
+            }
+            else if(rand === 3 && Num == true){
+              //Numeric
+              password += Math.floor(Math.random() * 10);
+              x = false;
+            }
+            else if (rand === 4 && SpeC == true){
+              //Special Character
+              password +=  SpeChar[Math.floor(Math.random() * SpeChar.length)];
+              x = false;
+            }
+            else{
+              // rand was not a character confirmed by user. Choose new rand.
+              rand = Math.ceil(Math.random() * 4);
+              x = true;
+            }
+          }
         }
-        else if(rand === 2){
-          //Lowercase letter
-          var x = parseInt(Math.floor(Math.random() * LowCase.length));
+        run = false
+      }
 
-          password +=  LowCase[x];
-          console.log(password);
-        }
-        else if(rand === 3){
-          //Lowercase letter
-          var x = Math.floor(Math.random() * 10);
 
-          password += x;
-          console.log(password);
-        }
-        else{
-         //Lowercase letter
-          var x = Math.ceil(Math.random() * SpeChar.length);
-
-          password +=  SpeChar[x];
-          console.log(password);
-        }
+      else{
+        // User did not confirm at least one character
+        run = confirm("Invalid Entry: Select at least one character type\n\nWould you still like to create a password? Yes(OK) No(Cancel)");
       }
     }
+
     else{
-      alert("Invalid Entry: Select at least one character type");
-      run = confirm("Would you like to create a password? Yes(OK) No(Cancel)");
+    // Password length entry was incorrect
+      run = confirm("Invalid Entry: Enter an number from 8 to 128\n\nWould you still like to create a password? Yes(OK) No(Cancel)");
     }
-  }
-  else{
-    alert("Invalid Entry");
-    run = confirm("Would you like to create a password? Yes(OK) No(Cancel)");
-  }
   }
   return password
 }
 
-document.getElementById("generate").addEventListener("click", generatePassword);
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
