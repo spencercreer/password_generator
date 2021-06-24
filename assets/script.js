f29090eb09b7c42fe3f00997c200d155bd4af9ab
spencercreer <spencercreer@gmail.com>
2021-06-23 21:13:11 -0600

    changed alerts to input elements, changed to jQuery

--- assets/script.js
+++ assets/script.js
@@ -1,91 +1,79 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+$(document).ready(function () {
+  // Assignment Code
+  var generateBtn = $("#generate");
 
   // Uppercase, lowercase, and special character arrays
   var UpCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var LowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var SpeChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = $("#password");
 
-  passwordText.value = password;
+    passwordText.html(password);
 
-}
+  }
 
-//generatePassword function
-function generatePassword(){
-  var run = true;
-  var password = "";
+  //generatePassword function
+  function generatePassword() {
+    var run = true;
+    var password = "";
+    var length = $("#length").val();
+    var LowC = $("#lowercase").is(":checked");
+    var UpC = $("#uppercase").is(":checked");
+    var Num = $("#numeric").is(":checked");
+    var SpeC = $("#specialChar").is(":checked");
 
-  while(run === true){
-
-    // Prompt user for password length
-    length = prompt("Enter your desired password length. (Enter a number from 8 to 128 characters)");
-  
     // Password length was correct
-    if(length >= 8 && length <= 128){
-
-      // Confirm user's desired password characters
-      LowC = confirm("Lowercase: Would you like your password to include lowercase characters? \n\nYes(OK) No(Cancel)");
-      UpC = confirm("Uppercase: Would you like your password to include uppercase characters? \n\nYes(OK) No(Cancel)");
-      Num = confirm("Numeric: Would you like your password to include numeric characters? \n\nYes(OK) No(Cancel)");
-      SpeC = confirm("Special characters: Would you like your password to include special characters? \n\nYes(OK) No(Cancel)");
-
+    if (length >= 8 && length <= 128) {
       // User selected at least one character
-      if(LowC == true || UpC == true || Num == true || SpeC == true){
-        for(i = 0; i < length; i++){
-          
+      if (LowC == true || UpC == true || Num == true || SpeC == true) {
+        for (i = 0; i < length; i++) {
+
           rand = Math.ceil(Math.random() * 4);
           x = true;
 
-          while(x == true){
-            if(rand === 1 && UpC == true){
+          while (x == true) {
+            if (rand === 1 && UpC == true) {
               //Uppercase letter
-              password +=  UpCase[Math.floor(Math.random() * UpCase.length)];
+              password += UpCase[Math.floor(Math.random() * UpCase.length)];
               x = false;
             }
-            else if(rand === 2 && LowC == true){
+            else if (rand === 2 && LowC == true) {
               //Lowercase letter
-              password +=  LowCase[Math.floor(Math.random() * LowCase.length)];
+              password += LowCase[Math.floor(Math.random() * LowCase.length)];
               x = false;
             }
-            else if(rand === 3 && Num == true){
+            else if (rand === 3 && Num == true) {
               //Numeric
               password += Math.floor(Math.random() * 10);
               x = false;
             }
-            else if (rand === 4 && SpeC == true){
+            else if (rand === 4 && SpeC == true) {
               //Special Character
-              password +=  SpeChar[Math.floor(Math.random() * SpeChar.length)];
+              password += SpeChar[Math.floor(Math.random() * SpeChar.length)];
               x = false;
             }
-            else{
+            else {
               // rand was not a character confirmed by user. Choose new rand.
               rand = Math.ceil(Math.random() * 4);
               x = true;
             }
           }
         }
-        run = false
-      }
-
-
-      else{
+      } else {
         // User did not confirm at least one character
-        run = confirm("Invalid Entry: Select at least one character type\n\nWould you still like to create a password? Yes(OK) No(Cancel)");
+        alert("Invalid Entry: Check at least one password criteria");
       }
+    } else {
+      // Password length entry was incorrect
+      alert("Invalid Entry: Enter an password length from 8 to 128")
     }
-
-    else{
-    // Password length entry was incorrect
-      run = confirm("Invalid Entry: Enter an number from 8 to 128\n\nWould you still like to create a password? Yes(OK) No(Cancel)");
-    }
+    return password
   }
-  return password
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.on("click", writePassword);
+})
