42cb78d2f233f77cc0e0077d402b442a9abfd0e7
spencercreer <spencercreer@gmail.com>
2021-09-22 22:54:05 -0700

    changed var to let and const

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,33 @@
 $(document).ready(function () {
-  var generateBtn = $("#generate"); // generate button
+  const generateBtn = $("#generate"); // generate button
 
   function writePassword() {
-    var passwordLength = $("#length").val();
-    if (passwordLength >= 8 && passwordLength <= 128) {
-      // valid length
-      validChars = getUserInputs(); // returns an array of valid characters
+    let passwordText = $("#password");
+    passwordText.html("");
+
+    let passwordLength = $("#length").val();
+
+    let lowerCaseConfirm = $("#lowercase").is(":checked");
+    let upperCaseConfirm = $("#uppercase").is(":checked");
+    let numberConfirm = $("#numeric").is(":checked");
+    let specialConfirm = $("#specialChar").is(":checked");
+
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("Invalid Length: Enter a number from 8 to 128"); // invalid length entry
+    } else if (!lowerCaseConfirm && !upperCaseConfirm && !numberConfirm && !specialConfirm) {
+      alert("Invalid Entry: Select at least one character type"); // no character type checked
+    } else {
+      // valid criteria
+      let validChars = getUserInputs(lowerCaseConfirm, upperCaseConfirm, numberConfirm, specialConfirm); // returns an array of valid characters
       let password = generatePassword(passwordLength, validChars); // returns password as a string
 
       // write generated password to html
-      var passwordText = $("#password");
       passwordText.html(password);
-    } else {
-      // invalid length entry
-      alert("Invalid Length: Enter an number from 8 to 128");
     }
   }
 
-  //generatePassword function
   function generatePassword(passwordLength, validChars) {
-    var password = "";
+    let password = "";
     for (let i = 0; i < passwordLength; i++) {
       let randomIndex = Math.floor(Math.random() * validChars.length);
       let randomChar = validChars[randomIndex];
@@ -28,40 +36,28 @@ $(document).ready(function () {
     return password;
   }
 
-  function getUserInputs() {
+  function getUserInputs(lowerCaseConfirm, upperCaseConfirm, numberConfirm, specialConfirm) {
     let validChars = [];
 
-    // confirm user's desired password characters
-    let lowerCaseConfirm = $("#lowercase").is(":checked");
-    let upperCaseConfirm = $("#uppercase").is(":checked");
-    let numberConfirm = $("#numeric").is(":checked");
-    let specialConfirm = $("#specialChar").is(":checked");
-
-    if (!lowerCaseConfirm && !upperCaseConfirm && !numberConfirm && !specialConfirm) {
-      // invalid user entry
-      alert("Invalid Entry: Select at least one character type");
-    } else {
-      // valid entry
-      if (lowerCaseConfirm) {
-        let lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-        validChars = validChars.concat(lowerCaseChars);
-      }
-      if (upperCaseConfirm) {
-        let upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-        validChars = validChars.concat(upperCaseChars);
-      }
-      if (numberConfirm) {
-        let numberChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-        validChars = validChars.concat(numberChars);
-      }
-      if (specialConfirm) {
-        let specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-        validChars = validChars.concat(specialChars);
-      }
-      return validChars;
+    if (lowerCaseConfirm) {
+      let lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+      validChars = validChars.concat(lowerCaseChars);
     }
+    if (upperCaseConfirm) {
+      let upperCaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+      validChars = validChars.concat(upperCaseChars);
+    }
+    if (numberConfirm) {
+      let numberChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+      validChars = validChars.concat(numberChars);
+    }
+    if (specialConfirm) {
+      let specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+      validChars = validChars.concat(specialChars);
+    }
+    return validChars;
   }
 
-  // Add event listener to generate button
+  // event listener on generate button
   generateBtn.on("click", writePassword);
 })
